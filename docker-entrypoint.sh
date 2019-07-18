65c90ffcda2c62f37109931d158d0d6c228425f5
Adam Tilghman <agt@ucsd.edu>
2019-07-18 14:54:40 -0700

    use env variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,11 +19,11 @@ openssl req -key helm.key.pem -new -sha256 -out helm.csr.pem -subj "/CN=tiller-u
 openssl x509 -req -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -in helm.csr.pem -out helm.cert.pem  -days 365
 
 
-CA_CERT=$(base64 < "./ca.key.pem" | tr -d '\n')
-TLS_KEY=$(base64 < "./tiller.key.pem" | tr -d '\n')
-TLS_CERT=$(base64 < "./tiller.cert.pem" | tr -d '\n')
+export CA_CERT=$(base64 < "./ca.key.pem" | tr -d '\n')
+export TLS_KEY=$(base64 < "./tiller.key.pem" | tr -d '\n')
+export TLS_CERT=$(base64 < "./tiller.cert.pem" | tr -d '\n')
 
 kubectl get secrets -n kube-system tiller-secret -o json |
-  jq '.data["ca.crt"] |= "$CA_CERT"' | \
-  jq '.data["tls.key"] |= "$TLS_KEY"' | \
-  jq '.data["tls.crt"] |= "$TLS_CERT"'
+  jq '.data["ca.crt"] |= "env.CA_CERT"' | \
+  jq '.data["tls.key"] |= "env.TLS_KEY"' | \
+  jq '.data["tls.crt"] |= "env.TLS_CERT"'
